a532ab1914689fb39b2ab5ade7688d2f84a2333c
Rodrigo <sanchez.moreno.rodrigo15@gmail.com>
2025-02-10 14:55:57 -0600

    Funcion Agregar amigos y limpiar caja

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaAmigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('nombreAmigo').value;
+
+    if (!(nombre === "")) {
+        listaAmigos.push(nombre);
+        limpiarCaja();
+  
+        console.log(listaAmigos);
+    } else {
+        alert("Por favor, inserte un nombre");
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#nombreAmigo').value = '';
+    return;
+}
