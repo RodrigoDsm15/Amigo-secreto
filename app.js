da560d22a71b16616ce88365e3da11ecd10727f8
Rodrigo <sanchez.moreno.rodrigo15@gmail.com>
2025-02-12 10:07:41 -0600

    crear funcion sortearAmigos

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//Variables
 //Lista amigos
 let listaAmigos = [];
-//Variable para agregar elemento a la lista HTML
+//Agregar elemento a la lista HTML
 let nuevoAmigo = document.getElementById('listaAmigos');
+//Nombre sorteado
+let nombreSorteado = sortearAmigo;
 
 //Funcion para afregar amigo a la lista
 function agregarAmigo() {
     let nombre = document.getElementById('nombreAmigo').value;
     //Estado de la caja
     if (!(nombre === "")) {
-        listaAmigos.push(nombre);
+        if (!(listaAmigos.includes(nombre))) {
+            listaAmigos.push(nombre);
+        } else {
+            alert("Ese nombre ya esta agregado");
+        }
         limpiarCaja();
         actualizarListaAmigos();
         console.log(listaAmigos);
     } else {
         alert("Por favor, inserte un nombre");
-        listaAmigos.innerHTML = "";
     }
     return;
 }
@@ -36,16 +42,17 @@ function actualizarListaAmigos() {
         elementoHTML.innerHTML = listaAmigos[i];
         nuevoAmigo.appendChild(elementoHTML);
     }
-}
-
-/*
-function asignarTextoElemento(elemento) {
-    listaAmigo.innerHTML = '';
-    for (let i = 0; i < listaAmigo.length; i++) {
-        let elementoHTML = document.createElement(elemento);
-        elementoHTML.innerHTML = listaAmigo[i];
-        document.getElementById("listaAmigos").appendChild(elementoHTML);
-    }
     return;
 }
-    */
+
+//Funcion para conocer a el amigo secreto
+function sortearAmigo() {
+    if (listaAmigos.length > 0) {
+        let numeroGenerado = listaAmigos[Math.floor(Math.random()*(listaAmigos.length))];
+        nuevoAmigo.innerHTML = '';
+        let nombreDeAmigoSecreto = document.querySelector("#resultado");
+        nombreDeAmigoSecreto.innerHTML = `Tu amigo secreto es: ${numeroGenerado}`;
+        console.log(numeroGenerado);
+        return numeroGenerado;
+    }
+}
