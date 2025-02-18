d6baa1ebc38fde7d546396d6ed121407e2f6d9d9
Rodrigo <sanchez.moreno.rodrigo15@gmail.com>
2025-02-18 16:38:53 -0600

    Desabilitar botones e input para nuevo juego

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 //Variables
-//Lista amigos
+//Lista de amigos
 let listaAmigos = [];
 //Agregar elemento a la lista HTML
 let nuevoAmigo = document.getElementById('listaAmigos');
-//Nombre sorteado
-let nombreSorteado = sortearAmigo;
 
 //Funcion para afregar amigo a la lista
 function agregarAmigo() {
     let nombre = document.getElementById('nombreAmigo').value;
-    //Estado de la caja
+    //Verificar que la caja no este vacia
     if (!(nombre === "")) {
+        //Verificar que el nombre no este en la lista
         if (!(listaAmigos.includes(nombre))) {
             listaAmigos.push(nombre);
         } else {
@@ -20,7 +17,7 @@ function agregarAmigo() {
         }
         limpiarCaja();
         actualizarListaAmigos();
-        console.log(listaAmigos);
+        //console.log(listaAmigos);
     } else {
         alert("Por favor, inserte un nombre");
     }
@@ -35,7 +32,7 @@ function limpiarCaja() {
 
 //funcion que actualizar las lista de amigos en HTML
 function actualizarListaAmigos() {
-    nuevoAmigo.innerHTML = '';
+    limpiarListaHtml();
 
     for (let i = 0; i < listaAmigos.length; i++) {
         let elementoHTML = document.createElement('li');
@@ -45,14 +42,30 @@ function actualizarListaAmigos() {
     return;
 }
 
+//Limpiar la lista HTML
+function limpiarListaHtml() {
+    nuevoAmigo.innerHTML = '';
+    return;
+}
+
 //Funcion para conocer a el amigo secreto
 function sortearAmigo() {
-    if (listaAmigos.length > 0) {
-        let numeroGenerado = listaAmigos[Math.floor(Math.random()*(listaAmigos.length))];
-        nuevoAmigo.innerHTML = '';
+    //Verificar que se encuenrten dos o mas elementos para sortear
+    if (listaAmigos.length > 1) {
+        let nombreElegido = listaAmigos[Math.floor(Math.random()*(listaAmigos.length))];
+        limpiarListaHtml();
         let nombreDeAmigoSecreto = document.querySelector("#resultado");
-        nombreDeAmigoSecreto.innerHTML = `Tu amigo secreto es: ${numeroGenerado}`;
-        console.log(numeroGenerado);
-        return numeroGenerado;
+        nombreDeAmigoSecreto.innerHTML = `Tu amigo secreto es: ${nombreElegido}`;
+        //Desabilitar botones e input para no agregar mas amigos y no volver a sortear
+        desabilitarBoton("#botonAgregar");
+        desabilitarBoton("#botonSortear");
+        desabilitarBoton("#nombreAmigo");
+        limpiarCaja();
+        //console.log(numeroGenerado);
+        return;
     }
+}
+
+function desabilitarBoton(identificador) {
+    document.querySelector(identificador).setAttribute('disabled','true');
 }
